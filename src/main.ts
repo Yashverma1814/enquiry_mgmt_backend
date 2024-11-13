99e166f833dcfcb20f3fe5122378ada1cbea80c0
Yash Verma <yashv1752@gmail.com>
2024-11-13 17:15:32 +0530

    some changes made to main

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:"*",
-    methods:"GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials:true,
-  })
-  await app.listen(process.env.PORT ?? 3000);
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port);
+  console.log(`Server running on port `, port);
 }
 bootstrap();
